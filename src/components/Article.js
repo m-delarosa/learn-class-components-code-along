8e9c1751ee9143f6e30ca2253a2eb7764f1f295c
github username <github email address>
2020-06-01 16:32:13 -0600

    Initial commit

---
+++ src/components/Article.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Article extends React.Component {
+    render() {
+        return (
+            <div>
+                Dear Reader: Bjarne Stroustrup had the perfect lecture oration.
+            </div>
+        )
+    }
+}
+
+export default Article
